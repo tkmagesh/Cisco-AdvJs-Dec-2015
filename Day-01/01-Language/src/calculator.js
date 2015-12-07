1d49dcd7cc2c541d737effdca7dd6fc510f38eb7
tkmagesh <tkmagesh77@gmail.com>
2015-12-07 13:10:03 +0530

    Language - Part - 1

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,20 @@
 function add(x,y){
-    return x + y;
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n);
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n,10);
+    }
+    var result = 0;
+    for(var i=0; i<arguments.length; i++)
+        result += parseArg(arguments[i]);
+    return result;
 }
+
+/*
+Function invocation
+* As a method of an obj
+    - this -> obj
+* As a function
+    - this -> global (window in the browser)
+
+*/
