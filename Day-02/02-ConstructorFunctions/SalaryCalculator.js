568e56b34b6788859727819c7c41e2ef134fad83
tkmagesh <tkmagesh77@gmail.com>
2015-12-08 12:11:50 +0530

    SalaryCalculator - Assignment

---
+++ Day-02/02-ConstructorFunctions/SalaryCalculator.js
@@ -0,0 +1,18 @@
+/*Create a SalaryCalculator 'class' that has the following attributes
+    - basic
+    - hra
+    - da
+    - tax
+    - salary
+
+    Note : tax - percentage, salary is initialize with 0
+
+    - calculate()
+        calculate the salary and update the 'salary' attribute accordingly
+
+    Usage :
+    var calc = new SalaryCalculator(10000,2000,3000,10)
+    calc.salary //=> 0;
+    calc.calcuate();
+    calc.salary //=> 13500;
+*/
